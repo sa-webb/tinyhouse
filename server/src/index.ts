fd9011d881f8db7150ab2782569fdefdf16ca250
Austin Webb <sawebb30@gmail.com>
2020-10-02 06:57:01 -0400

    apollo-server language

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,12 +1,12 @@
 import express from "express";
 import { ApolloServer } from "apollo-server-express";
-import { schema } from './graphql'
+import { typeDefs, resolvers } from './graphql'
 
 const port = 9000;
 
 const app = express();
 
-const server = new ApolloServer({ schema });
+const server = new ApolloServer({ typeDefs, resolvers });
 
 server.applyMiddleware({ app, path: "/api" });
 
