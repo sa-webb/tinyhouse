1062914081d2d7e7511a15bcb2c8ed1d5bf84d2b
Austin Webb <sawebb30@gmail.com>
2020-10-05 02:31:11 -0400

    feat: Listings

--- client/src/index.tsx
+++ client/src/index.tsx
@@ -1,12 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
 import * as serviceWorker from './serviceWorker';
+import {Listings} from './sections/Listings/Listings';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Listings title="TinyHouse Listings"/>
   </React.StrictMode>,
   document.getElementById('root')
 );
