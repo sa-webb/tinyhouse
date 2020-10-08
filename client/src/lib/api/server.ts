6d93d41ff04a6c74f9a08ea83c8904042aac31f2
Austin Webb <sawebb30@gmail.com>
2020-10-08 18:42:19 -0400

    error handling

--- client/src/lib/api/server.ts
+++ client/src/lib/api/server.ts
@@ -3,6 +3,10 @@ interface Body<TVariables> {
   variables?: TVariables;
 }
 
+interface Error {
+  message: string
+}
+
 export const server = {
   fetch: async <TData = any, TVariables = any>(body: Body<TVariables>) => {
     const res = await fetch("/api", {
@@ -12,7 +16,16 @@ export const server = {
       },
       body: JSON.stringify(body),
     });
+
+    if(res.status === 500) {
+      throw new Error("Something went wrong")
+    }
+
+    if(!res.ok) {
+      throw new Error("Cannot reach server")
+    }
+
     // type assertion
-    return res.json() as Promise<{ data: TData }>;
+    return res.json() as Promise<{ data: TData, errors: Error[] }>;
   },
 };
