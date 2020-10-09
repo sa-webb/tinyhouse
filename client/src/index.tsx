e86f3cf69dce211d3482b67175c3701cf6be7592
Austin Webb <sawebb30@gmail.com>
2020-10-08 23:02:21 -0400

    feat: ApolloClient

--- client/src/index.tsx
+++ client/src/index.tsx
@@ -1,17 +1,26 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import * as serviceWorker from './serviceWorker';
-import {Listings} from './sections/Listings/Listings';
+import React from "react";
+import ReactDOM from "react-dom";
+
+import "antd/dist/antd.css"
+
+import { Listings } from "./sections/Listings/Listings";
+
+import {
+  ApolloProvider,
+  ApolloClient,
+  InMemoryCache,
+} from "@apollo/client";
+
+const client = new ApolloClient({
+  uri: "/api",
+  cache: new InMemoryCache(),
+});
 
 ReactDOM.render(
   <React.StrictMode>
-    <Listings title="TinyHouse Listings"/>
+    <ApolloProvider client={client}>
+      <Listings title="TinyHouse Listings" />
+    </ApolloProvider>
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById("root")
 );
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
