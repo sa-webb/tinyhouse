6ed4cdb036dcba0f221080df09c65bcf450ab4de
Austin Webb <sawebb30@gmail.com>
2020-10-25 14:48:41 -0400

    mod2 typeDefs

--- server/src/@types/index.ts
+++ server/src/@types/index.ts
@@ -1,19 +1,62 @@
 import { Collection, ObjectId } from "mongodb";
 
+export interface BookingsIndexMonth {
+  [key: string]: boolean;
+}
+export interface BookingsIndexYear {
+  [key: string]: BookingsIndexMonth;
+}
+
+export interface BookingsIndex {
+  // KV index signature key - is a string and value - is another interface
+  [key: string]: BookingsIndexYear;
+}
+
+export interface Booking {
+  _id: ObjectId;
+  listing: ObjectId;
+  tenant: string;
+  checkIn: string;
+  checkOut: string;
+}
+
+export enum ListingType {
+  Apartment = "APARTMENT",
+  House = "HOUSE",
+}
 export interface Listing {
   _id: ObjectId;
   title: string;
+  description: string;
   image: string;
+  host: string;
+  type: ListingType;
   address: string;
+  country: string;
+  admin: string;
+  city: string;
+  bookings: ObjectId[];
+  bookingsIndex: BookingsIndex;
   price: number;
   numOfGuests: number;
-  numOfBeds: number;
-  numOfBaths: number;
-  rating: number;
+}
+
+export interface User {
+  _id: string;
+  token: string;
+  name: string;
+  avatar: string;
+  contact: string; // email
+  walletId: string;
+  income: number;
+  bookings: ObjectId[];
+  listings: ObjectId[];
 }
 
 export interface Database {
+  bookings: Collection<Booking>;
   listings: Collection<Listing>;
+  users: Collection<User>;
 }
 
 // Not reuseable
@@ -23,7 +66,7 @@ export interface Database {
 // identity(5);
 // identity("5");
 
-// Reuseable 
+// Reuseable
 // const identity = <T>(arg: T): T => {
 //     return arg;
 // }
