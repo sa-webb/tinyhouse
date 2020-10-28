07e7b1893c1aaef7656fa4887d9c3e963bc47a7c
Austin Webb <sawebb30@gmail.com>
2020-10-27 23:08:20 -0400

    feat: ReactRouterDom

--- client/src/index.tsx
+++ client/src/index.tsx
@@ -1,25 +1,52 @@
-import React from "react";
+import React, { useState } from "react";
 import ReactDOM from "react-dom";
 
-import "antd/dist/antd.css"
+import "./styles/index.css";
 
-import { Listings } from "./sections/Listings/Listings";
-
-import {
-  ApolloProvider,
-  ApolloClient,
-  InMemoryCache,
-} from "@apollo/client";
+import { ApolloProvider, ApolloClient, InMemoryCache } from "@apollo/client";
+import { Route, BrowserRouter as Router, Switch } from "react-router-dom";
+import { Login } from "./sections/Login";
+import { Layout } from "antd";
+import { Viewer } from "./lib/types";
+import { User } from "./sections/User";
 
 const client = new ApolloClient({
   uri: "/api",
   cache: new InMemoryCache(),
 });
 
+const initialViewer: Viewer = {
+  id: null,
+  token: null,
+  avatar: null,
+  hasWallet: null,
+  didRequest: false,
+};
+
+const App = () => {
+  const [viewer, setViewer] = useState<Viewer>(initialViewer);
+  return (
+    <Router>
+      <Layout id="app">
+        <Switch>
+          {/* ... */}
+          <Route
+            exact
+            path="/login"
+            render={(props) => <Login {...props} setViewer={setViewer} />}
+          />
+          <Route exact path="/user/:id" component={User} />
+          {/* ... */}
+        </Switch>
+      </Layout>
+    </Router>
+  );
+};
+
 ReactDOM.render(
   <React.StrictMode>
     <ApolloProvider client={client}>
-      <Listings title="TinyHouse Listings" />
+      <App />
     </ApolloProvider>
   </React.StrictMode>,
   document.getElementById("root")
