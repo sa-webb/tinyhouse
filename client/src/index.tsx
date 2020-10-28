c03b9159e67c001e00650ad6e920e1445631833c
Austin Webb <sawebb30@gmail.com>
2020-10-28 16:59:40 -0400

    feat: AppHeader

--- client/src/index.tsx
+++ client/src/index.tsx
@@ -6,9 +6,10 @@ import "./styles/index.css";
 import { ApolloProvider, ApolloClient, InMemoryCache } from "@apollo/client";
 import { Route, BrowserRouter as Router, Switch } from "react-router-dom";
 import { Login } from "./sections/Login";
-import { Layout } from "antd";
 import { Viewer } from "./lib/types";
 import { User } from "./sections/User";
+import { Affix } from "antd";
+import { AppHeader } from "./sections/AppHeader";
 
 const client = new ApolloClient({
   uri: "/api",
@@ -27,18 +28,19 @@ const App = () => {
   const [viewer, setViewer] = useState<Viewer>(initialViewer);
   return (
     <Router>
-      <Layout id="app">
-        <Switch>
-          {/* ... */}
-          <Route
-            exact
-            path="/login"
-            render={(props) => <Login {...props} setViewer={setViewer} />}
-          />
-          <Route exact path="/user/:id" component={User} />
-          {/* ... */}
-        </Switch>
-      </Layout>
+      <Affix offsetTop={0} className="app__affix-header">
+        <AppHeader viewer={viewer} setViewer={setViewer}/>
+      </Affix>
+      <Switch>
+        {/* ... */}
+        <Route
+          exact
+          path="/login"
+          render={(props) => <Login {...props} setViewer={setViewer} />}
+        />
+        <Route exact path="/user/:id" component={User} />
+        {/* ... */}
+      </Switch>
     </Router>
   );
 };
