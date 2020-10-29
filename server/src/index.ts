da36a7def6044cec36fbfadf7495371c10398d61
Austin Webb <sawebb30@gmail.com>
2020-10-28 21:20:30 -0400

    cookie

--- server/src/index.ts
+++ server/src/index.ts
@@ -3,14 +3,17 @@ import { ApolloServer } from "apollo-server-express";
 import express, { Application } from "express";
 import { connectDatabase } from "./database";
 import { resolvers, typeDefs } from "./graphql";
+import cookieParser from "cookie-parser";
 
 const mount = async (app: Application) => {
   const db = await connectDatabase();
 
+  app.use(cookieParser(process.env.SECRET));
+
   const server = new ApolloServer({
     typeDefs,
     resolvers,
-    context: () => ({ db }),
+    context: ({ req, res }) => ({ db, req, res }),
   });
 
   server.applyMiddleware({ app, path: "/api" });
