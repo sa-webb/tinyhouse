aacac41b7d224fdebdfe96c4635bde711f2b37cd
Austin Webb <sawebb30@gmail.com>
2020-10-30 18:34:49 -0400

    initial

---
+++ server/src/graphql/resolvers/Booking/index.ts
@@ -0,0 +1,18 @@
+import { IResolvers } from "apollo-server-express";
+import { Booking, Database, Listing } from "../../../lib/types";
+
+export const bookingResolvers: IResolvers = {
+  Booking: {
+    id: (booking: Booking): string => {
+      return booking._id.toString();
+    },
+    listing: (
+      booking: Booking,
+      // eslint-disable-next-line @typescript-eslint/ban-types
+      _args: {},
+      { db }: { db: Database }
+    ): Promise<Listing | null> => {
+      return db.listings.findOne({ _id: booking.listing });
+    },
+  },
+};
