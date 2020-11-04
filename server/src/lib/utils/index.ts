573574fa463e2049a092851e968ab15de4d652d8
Austin Webb <sawebb30@gmail.com>
2020-11-04 08:13:23 -0500

    findOne _id only

--- server/src/lib/utils/index.ts
+++ server/src/lib/utils/index.ts
@@ -5,11 +5,10 @@ export const authorize = async (
   db: Database,
   req: Request
 ): Promise<User | null> => {
-  const token = req.get("X-CSRF-TOKEN");
+  
   const viewer = await db.users.findOne({
     _id: req.signedCookies.viewer,
-    token,
   });
-
+  console.log(viewer)
   return viewer;
 };
