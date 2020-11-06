0a58aa46ae28f16dfa685fff119ab1331a3547e8
Austin Webb <sawebb30@gmail.com>
2020-11-06 11:16:45 -0500

    charge

--- server/src/lib/api/Stripe.ts
+++ server/src/lib/api/Stripe.ts
@@ -7,11 +7,32 @@ const client = new stripe(secret!, { apiVersion: "2020-08-27" });
 
 export const Stripe = {
   connect: async (code: string) => {
-    const response = await client.oauth.token( {
+    const response = await client.oauth.token({
       grant_type: "authorization_code",
       code,
     });
 
     return response;
+  },
+  charge: async (amount: number, source: string, stripeAccount: string) => {
+    try {
+      const res = await client.charges.create(
+        {
+          amount,
+          currency: "usd",
+          source,
+          application_fee_amount: Math.round(amount * 0.05),
+        },
+        {
+          stripeAccount: stripeAccount,
+        }
+      );
+
+      if (res.status !== "succeeded") {
+        throw new Error("failed to create charge with Stripe");
+      }
+    } catch (error) {
+      console.log(error);
+    }
   },
 };
