9ada6e8236a35622a2ecb5dc4ddc524c3f22f308
Austin Webb <sawebb30@gmail.com>
2020-11-30 09:18:11 -0500

    refact: clean

--- server/src/lib/api/Cloudinary.ts
+++ server/src/lib/api/Cloudinary.ts
@@ -5,7 +5,7 @@ export const Cloudinary = {
     const res = await cloudinary.v2.uploader.upload(image, {
       api_key: process.env.CLOUDINARY_KEY,
       api_secret: process.env.CLOUDINARY_SECRET,
-      cloud_namme: process.env.CLOUDINARY_NAME,
+      cloud_name: process.env.CLOUDINARY_NAME,
       folder: "TH_Assets/",
     // eslint-disable-next-line @typescript-eslint/no-empty-function
     }, function(){});
