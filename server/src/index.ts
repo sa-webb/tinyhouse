13c87f137bc8de3b31d8d4735a56691eafce44d6
Austin Webb <sawebb30@gmail.com>
2020-11-30 09:21:03 -0500

    client __dir

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,16 +1,21 @@
-require("dotenv").config();
 import { ApolloServer } from "apollo-server-express";
 import express, { Application } from "express";
 import { connectDatabase } from "./database";
 import { resolvers, typeDefs } from "./graphql";
 import cookieParser from "cookie-parser";
 import bodyParser from "body-parser";
+import compression from "compression";
 
 const mount = async (app: Application) => {
   const db = await connectDatabase();
 
   app.use(bodyParser.json({ limit: "2mb" }));
   app.use(cookieParser(process.env.SECRET));
+  app.use(compression());
+
+  app.use(express.static(`${__dirname}/client`));
+  // Single page application regardless of route - same index file.
+  app.get("/*", (_req, res) => res.sendFile(`${__dirname}/client/index.html`));
 
   const server = new ApolloServer({
     typeDefs,
